b14a0bca8d7b9d931997d81191e5095145152dd3
Maarten van der Meulen <mjvandermeulen@gmail.com>
2022-04-24 18:03:19 -0400

    milestone before js -> ts

--- scratchpad.js
+++ scratchpad.js
@@ -1,3 +1,20 @@
-import { random_words } from "./library.js";
+// // // const good = randomWords(4);
+// // // const bad = randomWords(1, true);
+// // const realLife = randomWords(3) + maybeABadWord();
 
-console.log(random_words(4));
+// // // console.log({ good });
+// // // console.log({ bad });
+// // console.log({ realLife });
+
+// // // console.log(hasBadWords(good));
+// // // console.log(hasBadWords(bad));
+// // console.log(hasBadWords(realLife));
+// console.log("boo");
+// const numberOfWords = 2;
+// const steps = 11;
+// for (let i = 0; i <= steps - 1; i++) {
+//   const totalChance = i / (steps - 1);
+//   const chance = 1 - Math.pow(1 - totalChance, 1 / numberOfWords);
+//   console.log({ totalChance });
+//   console.log({ chance });
+// }
